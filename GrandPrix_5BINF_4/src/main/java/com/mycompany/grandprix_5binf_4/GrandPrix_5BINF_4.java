986b10fcb03aaf8fc7f50e31140a1cc816bc36de
FrankMisciaj <misciafranco928@gmail.com>
2024-01-12 15:59:48 +0100

    terza prova

--- GrandPrix_5BINF_4/src/main/java/com/mycompany/grandprix_5binf_4/GrandPrix_5BINF_4.java
+++ GrandPrix_5BINF_4/src/main/java/com/mycompany/grandprix_5binf_4/GrandPrix_5BINF_4.java
@@ -13,6 +13,6 @@ public class GrandPrix_5BINF_4 {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Come stai?");
+        System.out.println("Come vai bro?");
     }
 }
